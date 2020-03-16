57801ddacad0c9496c31c9a0a3aa986a9569e49d
oleoot <oleoot28@gmail.com>
2020-03-16 13:40:47 +0200

    Clean code & new media

--- js/script.js
+++ js/script.js
@@ -362,3 +362,44 @@ const sceneCertificatess = new ScrollMagic.Scene({
 })
     .setTween(certificates)
     .addTo(controllerCertificates);
+
+
+// ========= Nav slider ===================================================================================================
+const navSlide = () => {
+    const nav = document.querySelector(".nav");
+    const navBurger = document.querySelector(".nav-burger");
+    const navLinks = document.querySelectorAll(".nav a");
+    // Toggle Nav
+    navBurger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+
+        // Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+            }
+
+        });
+        // Burger animation
+        navBurger.classList.toggle("toggle");
+
+    });
+    //  close navbar
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', () => {
+            nav.classList.toggle("nav-active");
+            navBurger.classList.toggle("toggle");
+
+            navLinks.forEach((link, index) => {
+                link.style.animation = '';
+            });
+        })
+    }
+
+
+    )
+}
+
+navSlide();
---
+++ telegram.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$text = $_POST['text'];
+$token = "1094837590:AAHQkk5IV-p8P9n6BCV2SKR_1FcWJOZRqRw";
+$chat_id = "-451985032";
+$arr = array(
+  'Имя: ' => $name,
+  'Email:' => $email,
+  'Текст сообщения: ' => $text,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  echo "Ok"
+  header('Thank-you');
+} else {
+  echo "Error";
+}
+?>
